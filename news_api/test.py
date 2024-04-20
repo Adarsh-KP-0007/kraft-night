018ea79b701f219d566402e43dbc2400bc3e9297
adarsh <adarshnambiar0007@gmail.com>
2024-04-21 03:29:17 +0530

    newsapp and aylien

--- news_api/test.py
+++ news_api/test.py
@@ -1,8 +1,7 @@
-import json
-import requests
+content = "abc def ghe"
+s=content.split()
+print(s)
+content='AND'.join(content)
 
-page = requests.get('https://timesofindia.indiatimes.com/nri/us-canada-news/more-h-1b-visa-holders-are-switching-jobs-the-process-explained/articleshow/109460294.cms')
-
-res = json.load(page)
-print(res)
+print(content)
 
